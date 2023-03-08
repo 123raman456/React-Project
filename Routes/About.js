a18127af0ada181869d748aea2b11c0726d67b6c
Raman Verma <123955950+123raman456@users.noreply.github.com>
2023-03-08 22:34:20 +0530

    Create About.js

---
+++ Routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navigation/Navbar'
+import Footer from '../Downbar/Footer';
+
+
+const About = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default About
