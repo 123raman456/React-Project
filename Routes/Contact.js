1ebe990c7c398854fa3f81d569f4e8ed3f131f5a
Raman Verma <123955950+123raman456@users.noreply.github.com>
2023-03-09 12:21:06 +0530

    Update Contact.js

--- Routes/Contact.js
+++ Routes/Contact.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Footer from '../Downbar/Footer'
 import Navbar from '../Navigation/Navbar'
-
+import Contact2  from "../ContactMe/Contact2";
 
 const Contact = () => {
   return (
     <div>
       <Navbar/>
-      <Contact/>
+      <Contact2/>
       <Footer/>
     </div>
   )
