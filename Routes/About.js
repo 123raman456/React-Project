ec09893f11769aa8f053f082b73585c373aa0343
Raman Verma <123955950+123raman456@users.noreply.github.com>
2023-03-09 12:21:42 +0530

    Update About.js

--- Routes/About.js
+++ Routes/About.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../Navigation/Navbar'
+import About2C from '../AboutMe/About2C';
 import Footer from '../Downbar/Footer';
-
 
 const About = () => {
   return (
     <div>
       <Navbar/>
+      <About2C/>
       <Footer/>
     </div>
   )
