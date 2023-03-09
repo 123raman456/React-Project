f243ffeeb9e2e158e858bbfddc507d45553bc0b0
Raman Verma <123955950+123raman456@users.noreply.github.com>
2023-03-09 12:16:58 +0530

    Update Contact.js

--- Contacts/Contact.js
+++ Contacts/Contact.js
@@ -1,19 +1,34 @@
-import "./Contact.css";
+import "./Contact2.css";
 import React from "react";
+import {Link} from 'react-router-dom';
 
-const Contact = () => {
+
+const Contact2 = () => {
   return (
     <div className="form">
-      <form>
+    <h1>Contact Me</h1>
+      <form method="post">
         <label for="">Frist Name</label>
-        <input type="text" />
+        <input type="text" placeholder="Enter your name"/> <br/>
         <label for="">Last Name</label>
-        <input type="text" />
+        <input type="text" placeholder="Enter your Last name"/> <br/>
         <label for="">Email</label>
-        <input type="email" />
+        <input type="email" placeholder="abc@123gmail.com"/><br/>
+        <label for="">Password</label>
+        <input type="password" placeholder="******"></input> <br/>
+        <label for="">Phone</label>
+        <input type="phone" maxLength="10" placeholder="987654****"/><br/>
+        <label for="">DOB</label>
+        <input type="date"></input><br/>
+        <label for="">Address</label>
+        <textarea name="" id="" cols="40" rows="4"></textarea> <br/>
+        <label for="">Gender</label>
+        <input type="radio"/>Male
+        <input type="radio"/>Female <br/>
+        <Link to="/" className="btn5">Submit</Link>
       </form>
     </div>
   );
 };
 
-export default Contact;
+export default Contact2;
